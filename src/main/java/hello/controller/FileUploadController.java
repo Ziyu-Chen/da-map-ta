240a5a1de843f8f82c6209a66c456948b85e1283
ziyu <zc839@nyu.edu>
2019-11-27 23:42:43 +0900

    Working on the upload endpoint

---
+++ src/main/java/hello/controller/FileUploadController.java
@@ -0,0 +1,59 @@
+package hello.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+// import java.io.InputStreamReader;
+// import java.io.BufferedReader;
+// import java.io.File;
+// import java.io.FileOutputStream;
+// import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class FileUploadController {
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
+	public String[][] fileUpload(@RequestParam("file") MultipartFile file,
+			@RequestParam(value = "cols", required = false) String cols) throws IOException {
+		String fileName = file.getOriginalFilename();
+
+		List<Integer> columns = Arrays.asList(cols.split(":")).stream().map(s -> Integer.valueOf(s))
+				.collect(Collectors.toList());
+
+		if (fileName.endsWith(".csv")) {
+			byte[] bytes = file.getBytes();
+			String completeData = new String(bytes);
+			String[] rows = completeData.split("\n");
+			int numCol = rows[0].split(",").length;
+			String[][] filteredRows = {};
+
+			for (int i = 0; i < rows.length; i++) {
+				String[] items = rows[i].split(",");
+				List<String> filteredRow = IntStream.range(0, numCol).filter(j -> columns.contains(j)).mapToObj(j -> items[j])
+						.collect(Collectors.toList());
+				filteredRows = ArrayUtils.add(filteredRows, filteredRow.toArray());
+			}
+
+			return filteredRows;
+		}
+		String[][] a = { { "a" } };
+		return a;
+	}
+
+	public String index() {
+		return "Fucking Shit!";
+	}
+
+}
