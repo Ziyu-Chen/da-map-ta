d9790f164e6bbfc449d618f8a3fb858f3d786b2f
ziyu <zc839@nyu.edu>
2019-11-28 11:51:48 +0900

    Fixed the endpoints. Renamed them table and columns

--- src/main/java/hello/controller/FileUploadController.java
+++ src/main/java/hello/controller/FileUploadController.java
@@ -3,7 +3,9 @@ package hello.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 // import java.io.InputStreamReader;
@@ -12,6 +14,7 @@ import java.util.Arrays;
 // import java.io.FileOutputStream;
 // import java.io.FileReader;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -24,39 +27,87 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class FileUploadController {
 
-	@RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	// public String[][] fileUpload(@RequestParam("file") MultipartFile file,
+	// @RequestParam(value = "cols", required = false) String cols) throws
+	// IOException {
+	// String fileName = file.getOriginalFilename();
 
-	public String[][] fileUpload(@RequestParam("file") MultipartFile file,
-			@RequestParam(value = "cols", required = false) String cols) throws IOException {
+	// List<Integer> columns = Arrays.asList(cols.split(":")).stream().map(s ->
+	// Integer.valueOf(s))
+	// .collect(Collectors.toList());
+
+	// if (fileName.endsWith(".csv")) {
+	// byte[] bytes = file.getBytes();
+	// String completeData = new String(bytes);
+	// String[] rows = completeData.split("\n");
+	// String[][] filteredRows = {};
+
+	// for (int i = 0; i < rows.length; i++) {
+	// String[] currentRow = rows[i].split(",");
+	// List<String> filteredRow = new ArrayList<String>();
+	// for (int j = 0; j < columns.size(); j++) {
+	// filteredRow.add(currentRow[columns.get(j)]);
+	// }
+	// filteredRows = ArrayUtils.add(filteredRows, filteredRow.toArray(new
+	// String[0]));
+	// }
+
+	// return filteredRows;
+	// }
+	// String[][] a = { { "a" } };
+	// return a;
+	// }
+	@RequestMapping(value = "/columns", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public List<String> getColumns(@RequestParam("file") MultipartFile file) throws IOException {
 		String fileName = file.getOriginalFilename();
-
-		List<Integer> columns = Arrays.asList(cols.split(":")).stream().map(s -> Integer.valueOf(s))
-				.collect(Collectors.toList());
-
 		if (fileName.endsWith(".csv")) {
 			byte[] bytes = file.getBytes();
-			String completeData = new String(bytes);
-			String[] rows = completeData.split("\n");
-			int numCol = rows[0].split(",").length;
-			String[][] filteredRows = {};
-
-			for (int i = 0; i < rows.length; i++) {
-				String[] currentRow = rows[i].split(",");
-				List<String> filteredRow = new ArrayList<String>();
-				for (int j = 0; j < columns.size(); j++) {
-					filteredRow.add(currentRow[columns.get(j)]);
-				}
-				filteredRows = ArrayUtils.add(filteredRows, filteredRow.toArray(new String[0]));
-			}
-
-			return filteredRows;
+			String dataString = new String(bytes);
+			String columnString = dataString.substring(0, dataString.indexOf("\n"));
+			return Arrays.asList(columnString.split(","));
 		}
-		String[][] a = { { "a" } };
+		List<String> a = new ArrayList<String>();
 		return a;
 	}
 
-	public String index() {
-		return "Fucking Shit!";
-	}
+	@RequestMapping(value = "/table", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public List<List<String>> getTable(@RequestParam("file") MultipartFile file,
+			@RequestParam(value = "cols", required = true) String cols) throws IOException {
+		String fileName = file.getOriginalFilename();
+		List<Integer> filteredColumns = Arrays.asList(cols.split(":")).stream().map(s -> Integer.valueOf(s))
+				.collect(Collectors.toList());
 
+		if (fileName.endsWith(".csv")) {
+			InputStreamReader isr = null;
+			BufferedReader br = null;
+			try {
+				isr = new InputStreamReader(file.getInputStream());
+				br = new BufferedReader(isr);
+				String line = null;
+				List<List<String>> strs = new ArrayList<List<String>>();
+
+				String[] columnNames = br.readLine().split(",");
+				List<String> filteredColumnNames = new ArrayList<String>();
+				for (int j = 0; j < filteredColumns.size(); j++) {
+					filteredColumnNames.add(columnNames[filteredColumns.get(j)]);
+				}
+				strs.add(filteredColumnNames);
+				while ((line = br.readLine()) != null) {
+					String[] currentRow = line.split(",");
+					if (currentRow.length != columnNames.length)
+						continue;
+					List<String> filteredRow = new ArrayList<String>();
+					for (int j = 0; j < filteredColumns.size(); j++) {
+						filteredRow.add(currentRow[filteredColumns.get(j)]);
+					}
+					strs.add(filteredRow);
+				}
+				return strs;
+			} catch (IOException e) {
+				System.out.println("Something is not right");
+			}
+		}
+		List<List<String>> a = new ArrayList<List<String>>();
+		return a;
+	}
 }
